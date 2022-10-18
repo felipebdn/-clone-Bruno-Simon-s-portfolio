41ab54262a301734535ddd2b24174d53f01e722f
Felipe Barbosa <71861370+felipebdn@users.noreply.github.com>
2022-10-18 20:41:14 -0300

    18102022

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { GlobalStyle } from "./Global/styles";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>
-)
+);
